7508906144a7aa6ee67777046631d093d0355aed
Morhaliuk Tetiana <svitprofservis@gmail.com>
2022-08-02 09:28:20 +0200

    read from file

--- MatchingGame/src/Main.java
+++ MatchingGame/src/Main.java
@@ -1,5 +1,24 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String filepath = System.getProperty("user.dir")
+                + "\\src\\data\\Words.txt";
+        ArrayList<String> data = new ArrayList<>();
+        try {
+            File myObj = new File(filepath);
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                data.add(myReader.nextLine());
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        data.forEach(System.out::println);
     }
 }
